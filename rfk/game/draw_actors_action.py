6c2fd0441ca604e5a1e774886e6c607c23917613
johnsondevin <91571848+johnsondevin@users.noreply.github.com>
2021-11-11 15:36:20 -0700

    Update draw_actors_action.py

--- rfk/game/draw_actors_action.py
+++ rfk/game/draw_actors_action.py
@@ -1,10 +1,13 @@
 from game.action import Action
 from output_service import OutputService
 
-class DrawActorsAction(OutputService):
+class DrawActorsAction(Action):
     
     def __init__(self, output_service):
-        _output() = output_service
+        self._output = output_service
 
-    def execute(self):
-        pass
+    def execute(self, cast):
+        self._output.clear_screen()
+        for i in cast:
+            self._output.draw_actor(i)
+            self._output.flush_buffer()
